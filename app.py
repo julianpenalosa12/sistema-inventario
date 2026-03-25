5e5529e01c8c8dbd579fbeb55de2376e54f4e532
julianpenalosa12 <julianpenalosa12@gmail.com>
2026-03-24 22:01:49 -0500

    Se añadio todo el tema de login con opciones dependiento cada rol, al iniciar la aplicacion inicia con login o dashcboard y se mejoro el diseño de varias paginas web del sistema

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from routes.usuario_routes import usuario_bp
 from routes.auth_routes import auth_bp
+from flask import session, redirect
 
 app = Flask(__name__)
 
@@ -11,7 +12,12 @@ app.register_blueprint(auth_bp)
 
 @app.route("/")
 def inicio():
-    return "Sistema de Inventario funcionando"
+    # Si ya está logeado → va al dashboard
+    if "usuario" in session:
+        return redirect("/dashboard")
+
+    # Si NO está logeado → va al login
+    return redirect("/login")
 
 if __name__ == "__main__":
     app.run(debug=True)
